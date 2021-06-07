00fd4fc54a35f570be21def35835cceaadcf89fd
Pumka <pupumka@users.noreply.github.com>
2021-06-08 00:31:30 +0300

    Initial commit

---
+++ src/main/java/com.example.ApplicationFiles/FileHandleApplication.java
@@ -0,0 +1,27 @@
+package com.example.ApplicationFiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+//import com.example.uploadingfiles.storage.StorageProperties;
+//import com.example.uploadingfiles.storage.StorageService;
+
+@SpringBootApplication
+//@EnableConfigurationProperties(StorageProperties.class)
+public class FileHandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileHandleApplication.class, args);
+    }
+
+    /*@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }*/
+}
