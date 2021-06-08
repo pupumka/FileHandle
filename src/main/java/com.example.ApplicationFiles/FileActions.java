01a03d86eae4f2097b08cbc567ed677007713671
Pumka <pupumka@users.noreply.github.com>
2021-06-09 01:05:20 +0300

    comit

--- src/main/java/com.example.ApplicationFiles/FileActions.java
+++ src/main/java/com.example.ApplicationFiles/FileActions.java
@@ -1,6 +1,7 @@
 package com.example.ApplicationFiles;
 
 
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -10,6 +11,8 @@ import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.*;
+import java.util.Arrays;
+
 import ru.ubmb.jstribog.StribogProvider;
 
 public class FileActions {
@@ -18,9 +21,12 @@ public class FileActions {
         if (Security.getProvider("JStribog") == null) {
             Security.addProvider(new StribogProvider());
         }
+        //File jpgFile = new File("C:\\Users\\pumka\\Desktop\\testFile.jpg");
+        //byte[] fileContent = Files.readAllBytes(jpgFile.toPath());
 
         MessageDigest md = MessageDigest.getInstance("Stribog512");
         byte[] digest = md.digest(multipartFile.getBytes());
+        //byte[] digest = md.digest(fileContent);
         return printHex(digest);
     }
 
@@ -37,8 +43,8 @@ public class FileActions {
     }
 
     private static File multypartToFile(MultipartFile file){
-        
-        return
+
+        return new File("");
     }
 
 }
